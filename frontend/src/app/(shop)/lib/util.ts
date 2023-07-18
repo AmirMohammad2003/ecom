74e8b77ddc3c8c8323159a577685ebf48bb0f4d6
AmirMohammad <68375124+AmirMohammad2003@users.noreply.github.com>
2023-07-18 17:58:40 +0330

    Add post to global fetchers.

--- frontend/src/app/(shop)/lib/util.ts
+++ frontend/src/app/(shop)/lib/util.ts
@@ -1,6 +1,15 @@
 export const API_URL = process.env.NEXT_PUBLIC_HOST_URL;
-export const fetcher = (input: string) => {
-  return fetch(API_URL + input, { credentials: "include" }).then((_res) => {
-    return _res.json();
-  });
+export const fetcher = (input: string) =>
+  _fetcher({ input: input, credentials: "include" });
+
+export const fetcherPost = (input: string) => _fetcher({input: input, credentials: "include", method: "post"});
+
+const _fetcher = ({
+  input,
+  ...args
+}: {
+  input: string;
+  args: { method: string; credentials: string };
+}) => {
+  return fetch(API_URL + input, { ...args }).then((_res) => _res.json());
 };
