11720aaf0c0a14a57fdede7372bac920e3af5b87
AmirMohammad <68375124+AmirMohammad2003@users.noreply.github.com>
2023-07-19 01:39:57 +0330

    Fix order endpoint

--- backend/orders/views.py
+++ backend/orders/views.py
@@ -1,6 +1,8 @@
+from rest_framework.exceptions import ValidationError
 from rest_framework.generics import CreateAPIView
 
 from cart.cart import Cart
+from shop.models import Product
 
 from .models import Order, OrderItem
 from .serializers import OrderSerializer
@@ -11,14 +13,18 @@ class OrderCreateView(CreateAPIView):
     serializer_class = OrderSerializer
 
     def perform_create(self, serializer):
+        cart = Cart(self.request)
+        if not cart.cart:
+            raise ValidationError("Cart is empty")
         serializer.save()
         order = Order.objects.get(pk=serializer.data["id"])
-        cart = Cart(self.request)
-        for key, item in cart.cart:
+        products = Product.objects.filter(id__in=cart.cart.keys())
+        for product in products:
+            product_id = str(product.pk)
             OrderItem.objects.create(
                 order=order,
-                product_id=key,
-                price=item["price"],
-                quantity=item["quantity"],
+                product_id=product.pk,
+                price=product.price,
+                quantity=cart.cart[product_id]["quantity"],
             )
         cart.clear()
