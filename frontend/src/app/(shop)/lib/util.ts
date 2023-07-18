dd5f9e1f699f4f307c7d2904f9dfa9a0d8cec03e
AmirMohammad <68375124+AmirMohammad2003@users.noreply.github.com>
2023-07-18 18:25:02 +0330

    Add post fetcher with json data

--- frontend/src/app/(shop)/lib/util.ts
+++ frontend/src/app/(shop)/lib/util.ts
@@ -2,14 +2,30 @@ export const API_URL = process.env.NEXT_PUBLIC_HOST_URL;
 export const fetcher = (input: string) =>
   _fetcher({ input: input, credentials: "include" });
 
-export const fetcherPost = (input: string) => _fetcher({input: input, credentials: "include", method: "post"});
+export const fetcherPost = (input: string) =>
+  _fetcher({ input: input, credentials: "include", method: "post" });
+
+export const fetcherPostJson = ({
+  input,
+  body,
+}: {
+  input: string;
+  body: Object;
+}) =>
+  _fetcher({
+    input: input,
+    credentials: "include",
+    method: "post",
+    body: JSON.stringify(body),
+    headers: { "Content-Type": "application/json" },
+  });
 
 const _fetcher = ({
   input,
   ...args
 }: {
   input: string;
-  args: { method: string; credentials: string };
+  args: { method: string; credentials: string; body?: string; headers?: any };
 }) => {
   return fetch(API_URL + input, { ...args }).then((_res) => _res.json());
 };
