dcbe58a647b1a2f0f8751d6084f7275962465d4c
AmirMohammad <68375124+AmirMohammad2003@users.noreply.github.com>
2023-07-19 17:42:04 +0330

    Add order details endpoint

--- backend/orders/urls.py
+++ backend/orders/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import OrderCreateView
+from .views import OrderCreateView, OrderDetailsApi
 
 app_name = "orders"
 
 urlpatterns = [
     path("order/create/", OrderCreateView.as_view(), name="order_create"),
+    path("order/", OrderDetailsApi.as_view(), name="order_details"),
 ]
--- backend/orders/views.py
+++ backend/orders/views.py
@@ -1,5 +1,8 @@
+from rest_framework import status
 from rest_framework.exceptions import ValidationError
 from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from cart.cart import Cart
 from orders.tasks import order_created
@@ -31,3 +34,33 @@ class OrderCreateView(CreateAPIView):
         cart.clear()
         order_created.delay(order.pk)
         self.request.session["order_id"] = order.pk
+
+
+class OrderDetailsApi(APIView):
+    def get(self, request):
+        order_id = request.session.get("order_id")
+        if not order_id:
+            return Response(
+                {"message": "No order found"}, status=status.HTTP_404_NOT_FOUND
+            )
+        order = Order.objects.get(pk=order_id)
+        if not order:
+            return Response(
+                {"message": "No order found"}, status=status.HTTP_404_NOT_FOUND
+            )
+        order_items = OrderItem.objects.filter(order=order)
+        order_items = [
+            {
+                "product": item.product.name,
+                "quantity": item.quantity,
+                "price": item.price,
+            }
+            for item in order_items
+        ]
+        return Response(
+            {
+                "items": order_items,
+                "total_price": order.get_total_cost(),
+            },
+            status=status.HTTP_200_OK,
+        )
