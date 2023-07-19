b36371b6dc22654f10e731f2fb4723400ef42e69
AmirMohammad <68375124+AmirMohammad2003@users.noreply.github.com>
2023-07-20 00:06:02 +0330

    Add details view in django admin

--- backend/onlineShop/urls.py
+++ backend/onlineShop/urls.py
@@ -3,7 +3,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+from orders.views import AdminOrderDetail
+
 urlpatterns = [
+    path("admin/order/<pk>/", AdminOrderDetail.as_view(), name="admin_order_detail"),
     path("admin/", admin.site.urls),
     path("v1/", include("shop.urls")),
 ]
