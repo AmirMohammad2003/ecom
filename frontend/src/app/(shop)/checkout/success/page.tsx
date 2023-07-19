c12b0851dd58478d8741d12a2bd7991a42e95025
AmirMohammad <68375124+AmirMohammad2003@users.noreply.github.com>
2023-07-19 23:14:42 +0330

    Enhance success page

--- frontend/src/app/(shop)/checkout/success/page.tsx
+++ frontend/src/app/(shop)/checkout/success/page.tsx
@@ -1,18 +1,47 @@
-import { redirect } from "next/navigation";
+"use client";
+import useSWR, { useSWRConfig } from "swr";
 import RedirectButton from "./components/redirect-button";
+import { useEffect, useState } from "react";
+import Loading from "../../components/loading";
+import { useRouter } from "next/navigation";
 
-const API_URL = process.env.API_URL;
+const API_URL = process.env.NEXT_PUBLIC_HOST_URL;
 const confirm = async () => {
-  return fetch(API_URL + "/payment/success/");
+  return fetch(API_URL + "/v1/payment/post-process/", {
+    credentials: "same-origin",
+  });
 };
 
-export default async function Success() {
-  const res = await confirm();
-  if (!res.ok) redirect("/");
+export default function Success() {
+  const router = useRouter();
+  const [loading, setLoading] = useState(true);
+  const { mutate } = useSWRConfig();
+  const { data, isLoading } = useSWR("confirm-payment", confirm);
+  useEffect(() => {
+    console.log(data);
+    if (data && data.ok) {
+      setLoading(false);
+    } else {
+      if (data && data.status === 401) router.push("/")
+      const id = setTimeout(() => {
+        mutate("confirm-payment");
+        clearTimeout(id);
+      }, 1000);
+    }
+  }, [data]);
   return (
-    <div className="flex flex-col text-5xl cart p-8 text-accent cursor-default w-full text-center h-34 rounded-box bg-base-300">
-      Payment Successful.
-      <RedirectButton />
+    <div className="flex flex-col items-center text-5xl cart p-8 text-accent cursor-default w-full text-center h-34 rounded-box bg-base-300">
+      {(loading && (
+        <>
+          <p className="mb-8">Processing Payment...</p>
+          <Loading />
+        </>
+      )) || (
+        <>
+          <p>Payment Successful.</p>
+          <RedirectButton />
+        </>
+      )}
     </div>
   );
 }
