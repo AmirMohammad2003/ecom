e47e988f4a30308de4639b81baede663a2ad9376
AmirMohammad <68375124+AmirMohammad2003@users.noreply.github.com>
2023-07-19 18:04:24 +0330

    Fix a bug in order summary list component

--- frontend/src/app/(shop)/checkout/components/order-summary-list.tsx
+++ frontend/src/app/(shop)/checkout/components/order-summary-list.tsx
@@ -11,7 +11,7 @@ export default async function OrderSummaryList({
       <h2 className="text-xl text-primary text-center">Your Order</h2>
       <div className="divider"></div>
       <div className="box-border p-4">
-        {items.map((item: any, index: number) => {
+        {items?.map((item: any, index: number) => {
           return (
             <div key={index} className="flex text-md items-center ">
               <p className="flex-wrap">
